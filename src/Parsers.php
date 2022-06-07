a1d428432bee3e4e884e3e71c5b4efe683885551
Torchyk Mikolai <M1kolikus@yandex.by>
2022-06-07 22:14:56 +0300

    step 8

--- src/Parsers.php
+++ src/Parsers.php
@@ -14,5 +14,11 @@ function parsers(string $nameFile): array
         return Yaml::parseFile($nameFile);
     }
 
+    $content = file_get_contents($nameFile);
+
+    if ($content === false) {
+        return [];
+    }
+
     return json_decode(file_get_contents($nameFile), true);
 }
