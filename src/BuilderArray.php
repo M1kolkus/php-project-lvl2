81353bac1dce7182ade93731191559efe855fdb3
Torchyk Mikolai <M1kolikus@yandex.by>
2022-06-07 21:48:51 +0300

    step 8

--- src/BuilderArray.php
+++ src/BuilderArray.php
@@ -2,47 +2,55 @@
 
 namespace src\BuilderArray;
 
+use function Functional\sort;
+
 function buildDiff(array $arr1, array $arr2): array
 {
     $keys = array_merge(array_keys($arr1), array_keys($arr2));
     $newKeys = array_unique($keys);
-    sort($newKeys);
+    $sortedKeys = sort($newKeys, fn($key1, $key2) => $key1 <=> $key2);
 
-    return array_reduce($newKeys, function (array $acc, string $key) use ($arr1, $arr2) {
+    return array_reduce($sortedKeys, function (array $acc, string $key) use ($arr1, $arr2) {
         $existsInFirstArray = array_key_exists($key, $arr1);
         $existsInSecondArray = array_key_exists($key, $arr2);
 
-        $node = ['key' => $key];
         $value = array_key_exists($key, $arr2) ? $arr2[$key] : $arr1[$key];
 
         if (is_array($value)) {
-            $node['type'] = 'object';
+            $type = 'object';
             $value = buildDiff($value, $value);
         } else {
-            $node['type'] = 'simple';
+            $type = 'simple';
         }
 
         if ($existsInFirstArray && $existsInSecondArray) {
             if (is_array($arr1[$key]) && is_array($arr2[$key])) {
-                $node['operation'] = 'not_changed';
-                $value = buildDiff($arr1[$key], $arr2[$key]);
+                $operation = 'not_changed';
+                $valueChildren = buildDiff($arr1[$key], $arr2[$key]);
             } elseif ($arr1[$key] === $arr2[$key]) {
-                $node['operation'] = 'not_changed';
+                $operation = 'not_changed';
             } else {
-                $node['operation'] = 'changed';
-                $node['oldValue'] = is_array($arr1[$key]) ? buildDiff($arr1[$key], $arr1[$key]) : $arr1[$key];
+                $operation = 'changed';
+                $oldValue = is_array($arr1[$key]) ? buildDiff($arr1[$key], $arr1[$key]) : $arr1[$key];
 
                 if (is_array($arr1[$key]) !== is_array($arr2[$key])) {
-                    $node['oldType'] = is_array($arr1[$key]) ? 'object' : 'simple';
+                    $oldType = is_array($arr1[$key]) ? 'object' : 'simple';
                 }
             }
         } elseif ($existsInFirstArray) {
-            $node['operation'] = 'disappeared';
+            $operation = 'disappeared';
         } else {
-            $node['operation'] = 'appeared';
+            $operation = 'appeared';
         }
 
-        $node['value'] = $value;
+        $node = [
+            'key' => $key,
+            'type' => $type,
+            'oldType' => $oldType ?? null,
+            'operation' => $operation,
+            'value' => $valueChildren ?? $value,
+            'oldValue' => $oldValue ?? null,
+        ];
 
         return [...$acc, $node];
     }, []);
